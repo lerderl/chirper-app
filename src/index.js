f40ba837ce0bb99380bf5e910fe1a4a3aa197f3e
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-06-21 09:59:17 +0100

    Created the store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { legacy_createStore } from "redux";
+
 import "./index.css";
+import reducer from "./reducers";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = legacy_createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+);
