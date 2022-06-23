022f35c3967f140bcb20a984ac6f0f43cde335f6
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-06-23 08:32:36 +0100

    Completed the New Tweet UI

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -19,16 +19,24 @@ const NewTweet = () => {
     setText("");
   };
 
+  const tweetCharLeft = 280 - text.length;
+
   return (
     <div>
       <h3 className="center">Compose New Tweet</h3>
       <form className="new-tweet" onSubmit={handleSubmit}>
+        {/* TODO: Redirect to '/' if submitted */}
         <textarea
           value={text}
+          maxLength={280}
           onChange={handleChange}
-          className="textarea" maxLength={280}
+          className="textarea"
           placeholder="What do you have to say?"
         />
+        {tweetCharLeft <= 100 &&
+          <div className="tweet-length">{tweetCharLeft}</div>
+        }
+        <button className="btn" type="submit" disabled={text === ""}>Submit</button>
       </form>
     </div>
   );
